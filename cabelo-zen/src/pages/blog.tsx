d411f9ecbeffa6ba005e9ade60e0d6986c794acf
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2023-05-07 16:19:01 -0300

    💅🏻Pretty: Mudando diagramação do blog

--- cabelo-zen/src/pages/blog.tsx
+++ cabelo-zen/src/pages/blog.tsx
@@ -1,47 +1,56 @@
 import Link from "next/link";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import Comentarios from "../Components/Comentarios";
+import Image from "next/image";
 import { Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+
 
 type Blog = {
 
 };
-export default function Blog( {posts}: { posts: any,post:any } ){
+export default function Blog({ posts }: { posts: any, post: any }) {
   return (
-    
+
     <div >
-    <Header/>
-    <div className="dg-grid-blog">
-    {
-      
-      posts.nodes.map((post: { slug: Key | null | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; excerpt: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => {
-       
-        return(
-          
-          <div key={post.slug} className="posts">
-            <div>
-              <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-                <p> {post.excerpt}</p> 
+      <Header />
+      <div className="blog">
+        <div key={post.slug} className="posts">
+          <div>
+            <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+            <div
+              className="text-base text-grey-darker"
+              dangerouslySetInnerHTML={{ __html: post.excerpt }}>
+            </div>
+            <div className="botao-blog">
+              <Link href={`/posts/${post.slug}`}>Ler post</Link>
             </div>
           </div>
-        )
-      })
-    }
+          <div>
+            <img
+              className="w-full"
+              src={post.featuredImage?.node.sourceUrl}
+            />
+          </div>
+        </div>
+        <div className="sidebar">
+        </div>
+      </div>
+      <Comentarios/>
+      <Footer />
     </div>
-       <Footer/>
-  </div>
   );
-  
+
 }
 
-export async function getStaticProps(){
+export async function getStaticProps() {
 
-    const res = await fetch('https://wordpress.cabelozen.com.br/graphql', {
-        
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            query: `
+  const res = await fetch('https://wordpress.cabelozen.com.br/graphql', {
+
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      query: `
             query HomePageQuery {
               posts {
                 nodes {
@@ -52,17 +61,16 @@ export async function getStaticProps(){
               }
             }
             `,
-        })
     })
-  
-    const json = await res.json()
-  
-    return {
-      props: {
-          posts: json.data.posts,
-          
-      },
-    }
-  
+  })
+
+  const json = await res.json()
+
+  return {
+    props: {
+      posts: json.data.posts,
+
+    },
   }
-  
+
+}
