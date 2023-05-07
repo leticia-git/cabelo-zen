829fb878370626f70d5da90485b95a2f5e962b38
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2023-05-07 19:30:21 -0300

    💅🏻Pretty: Mudando diagramação do blog

--- cabelo-zen/src/pages/blog.tsx
+++ cabelo-zen/src/pages/blog.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import Comentarios from "../Components/Comentarios";
 import Image from "next/image";
 import { Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
 
@@ -49,6 +50,7 @@ export default function Blog( {posts}: { posts: any,post:any } ){
       })
     }
     </div>
+    <Comentarios/>
        <Footer/>
   </div>
   );
