445b6433588d87f03d30f2a4e768347959fcb7cb
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-05-02 21:24:09 -0300

    Atualizando dominio

--- cabelo-zen/src/pages/blog.js
+++ cabelo-zen/src/pages/blog.js
@@ -3,55 +3,59 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import Comentarios from "../Components/Comentarios";
 import Image from "next/image";
-import { Key, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
-import {getLatestPosts} from '../lib/api';
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+} from "react";
+import { getLatestPosts } from "../lib/api";
 
-
-export default function Blog( {posts} ){
-  console.log(posts)
+export default function Blog({ posts }) {
+  console.log(posts);
   return (
-    
-    <div >
-    <Header/>
-    <div className="dg-grid-blog">
-    {
-      posts.nodes.map(post=> {
-        return(
-          
-   
-          <div className="blog">
-          <div key={post.slug} className="posts">
-            <div>
-              <Link href={`/posts/${post.slug}`}>{post.title}</Link>
-             <article dangerouslySetInnerHTML={{__html: post.excerpt}}></article>
-                <div className="botao-blog">
-                <Link href={`/posts/${post.slug}`}>Ler post</Link>
+    <div>
+      <Header />
+      <div className="dg-grid-blog">
+        <div className="blog">
+          {posts.nodes.map((post) => {
+            return (
+              <div className="blog">
+                <div key={post.slug} className="posts">
+                  <div>
+                    <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+                    <article
+                      dangerouslySetInnerHTML={{ __html: post.excerpt }}
+                    ></article>
+                    <div className="botao-blog">
+                      <Link href={`/posts/${post.slug}`}>Ler post</Link>
+                    </div>
+                  </div>
                 </div>
-            </div>
-          </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="side">
           <div className="sidebar">
+            <p>Destaques</p>
+          </div>
+        </div>
+      </div>
 
-          </div>
-          </div>
-        )
-      })
-    }
+      <Comentarios />
+      <Footer />
     </div>
-    <Comentarios/>
-       <Footer/>
-  </div>
   );
-  
 }
 
-export async function getStaticProps(){
-
-    const res = await fetch('https://wordpress.cabelozen.com.br/graphql', {
-        
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            query: `
+export async function getStaticProps() {
+  const res = await fetch("https://wordpress.esteticaecabelozen.com.br/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `
             query HomePageQuery {
               posts {
                 nodes {
@@ -62,18 +66,14 @@ export async function getStaticProps(){
               }
             }
             `,
-        })
-    })
-  
-    const json = await res.json()
-  
-    return {
-      props: {
-          posts: json.data.posts,
-          
-      },
-    }
-  
-  }
-  
+    }),
+  });
 
+  const json = await res.json();
+
+  return {
+    props: {
+      posts: json.data.posts,
+    },
+  };
+}
